eaf7c5bc1b93598ff01b02156194d1739499f512
pch720 <52265916+pch720@users.noreply.github.com>
2021-09-19 09:50:44 +0000

    update 2021-09-19

--- WEB/military_vacation_system/src/views/Home.js
+++ WEB/military_vacation_system/src/views/Home.js
@@ -2,13 +2,17 @@ import React, { Fragment } from 'react';
 
 import Header from 'components/Header';
 import HomeDetail from 'components/HomeDetail';
+import Form from 'components/Form';
+import TodoItemList from 'components/TodoItemList';
 
 class Home extends React.Component {
     render() {
         return (
             <Fragment>
                 <Header />
-                <HomeDetail />
+                <HomeDetail form={<Form/>}>
+                    <TodoItemList/>
+                </HomeDetail>
             </Fragment>
         )
     };
