acee34f3b58aca8b3407f6f747fe3971b6e0341f
pch720 <52265916+pch720@users.noreply.github.com>
2021-10-01 14:57:44 +0000

    캘린더 추가

--- WEB/military_vacation_system/src/components/Navs/Navs.js
+++ WEB/military_vacation_system/src/components/Navs/Navs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 
 const handleSelect = (eventKey) => {
-  switch (eventKey) {
+  /*switch (eventKey) {
     case '1':
       document.getElementById('VacationList').style.display='none';
       break;
@@ -14,22 +14,32 @@ const handleSelect = (eventKey) => {
       break;
     default:
       document.getElementById('VacationList').style.display='none';
-  }
-
+  }*/
 };
 
 class Navs extends React.Component {
   render() {
+    let isLogin = sessionStorage.getItem('Auth');
     return (
         <Nav fill variant="tabs" defaultActiveKey="1" onSelect={handleSelect}>
         <Nav.Item>
-          <Nav.Link eventKey="1">휴가 신청</Nav.Link>
+        {isLogin == null
+          ?<Nav.Link eventKey="1" disabled>휴가 신청</Nav.Link>
+          :<Nav.Link eventKey="1">휴가 신청</Nav.Link>
+          }
+        </Nav.Item>
+        <Nav.Item>          
+          {isLogin == null
+          ?<Nav.Link eventKey="2" disabled>휴가 목록</Nav.Link>
+          :<Nav.Link eventKey="2">휴가 목록</Nav.Link>
+          }
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="2">휴가 목록</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="3">현황</Nav.Link>
+          {isLogin == null
+          ?<Nav.Link eventKey="3" disabled>현황</Nav.Link>
+          :<Nav.Link eventKey="3">현황</Nav.Link>
+          }
+          
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="4" disabled>
--- WEB/military_vacation_system/src/components/UseVac/UseVac.js
+++ WEB/military_vacation_system/src/components/UseVac/UseVac.js
@@ -1,37 +1,17 @@
-import React, { Component, Fragment } from 'react';
-import { Badge, Container, Row, Col } from 'reactstrap';
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
-class UseVac extends Component {
+
+class UseVac extends React.Component {
     render() {
         return (
-            <main className="my-5 py-5">
-                <Container className="px-0">
-                    <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
-                        <Col className="py-5 mb-5 py-md-0 mb-md-0">
-                            <Fragment>
-                                <div className="position-relative">
-
-                                    <span className="d-block pb-2 mb-0 h6 text-uppercase text-info font-weight-bold">
-                                        Project 1
-                                        <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>
-                                            New
-                                        </Badge>
-                                    </span>
-
-                                    <span className="d-block pb-4 h2 text-dark border-bottom border-gray">React Project</span>
-
-                                    <article className="pt-5 text-secondary text-justify" style={{ fontSize: '0.9rem', whiteSpace: 'pre-line' }}>
-                                        UUUUUUUUUUUsevac
-                                    </article>
-
-                                </div>
-                            </Fragment>
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
-        );
-    }
+          <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+          />
+        )
+      }
 }
 
 export default UseVac;
