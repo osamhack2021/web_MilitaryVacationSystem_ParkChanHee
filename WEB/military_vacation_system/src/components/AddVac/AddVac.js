2090cf29131432b732463a1559685660e7c3561c
pch720 <52265916+pch720@users.noreply.github.com>
2021-10-02 14:53:15 +0000

    update 2021-10-02

--- WEB/military_vacation_system/src/components/AddVac/AddVac.js
+++ WEB/military_vacation_system/src/components/AddVac/AddVac.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, Modal } from "react-bootstrap";
 import './AddVac.css';
 
-const AddVac = ({value, onChange, onCreate, onKeyPress}) => {
+const AddVac = ({ value, onChange, onCreate, onKeyPress }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="form">
-      <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
-      <div className="create-button" onClick={onCreate}>
+    <>
+      <Button variant="primary" onClick={handleShow}>
         등록
-      </div>
-    </div>
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>휴가 등록</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
--- WEB/military_vacation_system/src/views/Home.js
+++ WEB/military_vacation_system/src/views/Home.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import axios from 'axios';
 
 import Header from 'components/Navs/Header';
 import Login from 'components/Sidebar/Login';
